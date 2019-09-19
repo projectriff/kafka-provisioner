911db93cd3b83482dd2bbe198817da0dabeeca7e
Eric Bottard <ebottard@pivotal.io>
2019-09-19 13:59:05 +0200

    Make code more robust, use /ns/name in URL

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
-# kafka-provider
+# kafka-provisioner
+The Kafka Provisioner component is responsible for creating
+topics in a Kafka cluster when asked to do so by the 
+[riff system](https://github.com/projectriff/system).
 
-(work-in-progress)
+For a given riff `stream` "foo" existing in namespace "my-ns",
+a PUT request will be made to this component at `/my-ns/foo`.
+It will react by creating a topic named `my-ns_foo`.
+Note that the underscore is an allowed character in Kafka topic names
+but is disallowed is kubernetes resource names.
+This avoids collisions between `ns=foo-bar:stream=quizz` and 
+`ns=foo:stream=bar-quizz` 
 
-deploy the kafka-provider:
-
+Upon successful creation (or lookup of pre-existing) of a topic,
+it will return its [liiklus](https://github.com/bsideup/liiklus)
+coordinates in the following json form:
+ ```json
+{
+  "gateway": "<host>:<port>",
+  "topic": "<created-topic-name>"
+}
 ```
-GO111MODULE=on ko apply -f config/
-```
 
-forward localhost port 8080 to the deployment:
-
-```
-kubectl port-forward deployment/kafka-provider 8080:8080
-```
-
-create a topic:
-
-```
-curl -X PUT localhost:8080/providertest1
-```
+## Configuration
+The provisioner should run with the following environment variables
+configured:
+* `BROKER`: the address of a Kafka broker to connect to, in the form `host:port`
+* `GATEWAY`: the address of a liiklus gRPC endpoint. Will be used as part
+of the returned coordinates (see above).
 
--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -17,9 +17,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/Shopify/sarama"
 )
@@ -27,30 +30,74 @@ import (
 var (
 	config  = sarama.NewConfig()
 	gateway = os.Getenv("GATEWAY")
-	broker = os.Getenv("BROKER")
+	broker  = os.Getenv("BROKER")
 )
 
 func main() {
+
+	if gateway == "" {
+		log.Fatal("Environment variable GATEWAY should contain the host and port of a liiklus gRPC endpoint")
+	}
+	if broker == "" {
+		log.Fatal("Environment variable BROKER should contain the host and port of a Kafka broker")
+	}
+
 	config.Version = sarama.V0_11_0_0
 	admin, err := sarama.NewClusterAdmin([]string{broker}, config)
 	if err != nil {
 		panic(err)
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "PUT" {
+		if r.Method == http.MethodPut {
 			handlePut(w, r, admin)
 		} else {
-			w.WriteHeader(404)
+			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
 func handlePut(w http.ResponseWriter, r *http.Request, admin sarama.ClusterAdmin) {
-	topicName := r.URL.Path[1:]
+	parts := strings.Split(r.URL.Path[1:], "/")
+	if len(parts) != 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = fmt.Fprintf(w, "URLs should be of the form /<namespace>/<stream-name>")
+		return
+	}
+	// NOTE: choice of underscore as separator is important as it is not allowed in k8s names
+	topicName := fmt.Sprintf("%s_%s", parts[0], parts[1])
 	topicDetail := sarama.TopicDetail{NumPartitions: 1, ReplicationFactor: 1}
-	admin.CreateTopic(topicName, &topicDetail, false)
-	w.WriteHeader(201)
-	response := fmt.Sprintf("{\"gateway\":\"%s\",\"topic\":\"%s\"}", gateway, topicName)
-	w.Write([]byte(response))
+	if metadata, err := admin.DescribeTopics([]string{topicName}); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = fmt.Fprintf(os.Stderr, "Error trying to list topics to see if %q exists: %v\n", topicName, err)
+		_, _ = fmt.Fprintf(w, "Error trying to list topics to see if %q exists: %v\n", topicName, err)
+	} else if metadata[0].Err == sarama.ErrUnknownTopicOrPartition {
+		if err := admin.CreateTopic(topicName, &topicDetail, false); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = fmt.Fprintf(os.Stderr, "Error creating topic %q: %v\n", topicName, err)
+			_, _ = fmt.Fprintf(w, "Error creating topic %q: %v\n", topicName, err)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+	} else if metadata[0].Err == sarama.ErrNoError {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = fmt.Fprintf(os.Stderr, "Error creating topic %q: %v\n", topicName, err)
+		_, _ = fmt.Fprintf(w, "Error creating topic %q: %v\n", topicName, err)
+		return
+	}
+
+	// Either created or already existed
+	w.Header().Set("Content-Type", "application/json")
+	res := result{Gateway: gateway, Topic: topicName}
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed to write json response: %v", err)
+		return
+	}
+}
+
+type result struct {
+	Gateway string `json:"gateway"`
+	Topic   string `json:"topic"`
 }
